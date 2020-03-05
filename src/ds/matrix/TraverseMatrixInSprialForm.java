fe0cbd8161baa221d6daee4346ebc79b948d98b4
Randhir Raj <randraj@cisco.com>
2020-03-05 13:19:53 +0530

    added solution description

--- src/ds/matrix/TraverseMatrixInSprialForm.java
+++ src/ds/matrix/TraverseMatrixInSprialForm.java
@@ -21,6 +21,20 @@ package ds.matrix;
 	1 2 3 4 5 6 12 18 17 16 15 14 13 7 8 9 10 11
 	
 	link - https://www.geeksforgeeks.org/print-a-given-matrix-in-spiral-form/
+	
+	Soln - use i for row and j for column, 
+	so I have taken 
+	i1 for starting row 
+	i2 for ending row 
+	j1 for starting column
+	j2 for ending column
+	
+	1) start traversing from first row i.e from j1 to j2. you have traversed the i1 so increase it by 1.
+	2) now traverse last column i.e from i1 to i2, now you have traversed the j2 so decrease it by 1.
+	3) now traverse last row from j2 to j1, so basically you have travered the i2 so decrese it by 1.
+	4) now traverse the first column from i2 to i1, so basically you have traversed the j1 so increase it by 1.
+	5) Do from step 1 to 4 until i2 > i1
+	   
  */
 
 public class TraverseMatrixInSprialForm {
